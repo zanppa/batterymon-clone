699360627fb5d2342dc72eb99dd19494a3012405
Denilson Sá <denilsonsa@gmail.com>
2010-01-30 05:38:39 -0200

    Changed TABs to spaces and fixed indentation to 4 spaces.
    Now, the code conforms better to the PEP-8 style guide.
    http://www.python.org/dev/peps/pep-0008/
    
    This is basically the same patch as posted in
    http://code.google.com/p/batterymon/issues/detail?id=13

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 import logging
 class logger_init:
     def __init__(self):
-	self.mylogger = logging.getLogger("batterymon")
+        self.mylogger = logging.getLogger("batterymon")
         self.mylogger.setLevel(logging.INFO)
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
@@ -9,18 +9,16 @@ class logger_init:
         self.mylogger.addHandler(ch)
         formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         ch.setFormatter(formatter)
-    
-    
+
     def debug(self,message):
         self.mylogger.debug(message)
-    
+
     def error(self,message):
         self.mylogger.error(message)
-    
+
     def critical(self,message):
         self.mylogger.critical(message)
-    
-    def set_level(self,level):        
+
+    def set_level(self,level):
         if level == "debug":
             self.mylogger.setLevel(logging.DEBUG)
-#}}}
--- settings.py
+++ settings.py
@@ -6,35 +6,35 @@ import os
 
 class config:
     def __init__(self):
-	self.configure = ConfigParser.ConfigParser()
-	self.filename= self.settings_exist ()
-	#self.configure.read(self.filename)  ##should make sure no file is read
-	
+        self.configure = ConfigParser.ConfigParser()
+        self.filename= self.settings_exist ()
+        #self.configure.read(self.filename)  ##should make sure no file is read
+
     def settings_exist(self):
-	 if os.path.exists('~/.config/batterymon/batterymon.rc'):
-	     return "~/.config/batterymon/batterymon.rc"
-	 elif os.path.exists('/usr/share/batterymon/batterymon.rc'):
-	     return "/usr/share/batterymon/batterymon.rc"
-	 else:
-	     return ('./batterymon.rc')
-    
+         if os.path.exists('~/.config/batterymon/batterymon.rc'):
+             return "~/.config/batterymon/batterymon.rc"
+         elif os.path.exists('/usr/share/batterymon/batterymon.rc'):
+             return "/usr/share/batterymon/batterymon.rc"
+         else:
+             return ('./batterymon.rc')
+
     def read_settings(self):
-	self.configure.read (self.filename)
-	return ("done")
-    
+        self.configure.read (self.filename)
+        return ("done")
+
     def read_settings_int(self,key):
-	x = self.configure.get ("tray",key)
-        return int(x)    
-	
-	def write_settings_string(self,key,value):
-		self.configure.set("tray",key,str(value) )
-        	with open (self.filename, 'wb') as configfile:
-	           self.configure.write(configfile)
-		return
-		
-    def write_settings_int(self,key,value):	
-	self.configure.set("tray",key,str(value) )
+        x = self.configure.get ("tray",key)
+        return int(x)
+
+    def write_settings_string(self,key,value):
+        self.configure.set("tray",key,str(value) )
         with open (self.filename, 'wb') as configfile:
            self.configure.write(configfile)
-        return    
-	
+        return
+
+    def write_settings_int(self,key,value):
+        self.configure.set("tray",key,str(value) )
+        with open (self.filename, 'wb') as configfile:
+           self.configure.write(configfile)
+        return
+
