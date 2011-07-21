2b79a75f28b9d4280bf07e74cc6d9a0803f343c7
Denilson Sá <denilsonsa@gmail.com>
2011-07-21 03:34:09 -0300

    Updated setup.py script

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from DistUtilsExtra.command import *
 import glob
 from distutils.core import setup
 
 setup(name='batterymon',
-      version="1.2.4",
-      description='A GNOME applet to monitor battery usage',
-      author='Matthew Horsell',
-      author_email='matthew.horsell@gmail.com',
-      url='http://code.google.com/p/batterymon',
+      version="1.3.0",
+      description='A simple battery monitor for any desktop',
+      author='Denilson Sá',
+      author_email='denilsonsa@gmail.com',
+      url='https://github.com/denilsonsa/batterymon-clone',
       package_dir={'batterymon': ''},
       packages = ['batterymon'],
       scripts=['batterymon'],
-      data_files=[('share/batterymon/icons/gnome', 
-                glob.glob('icons/gnome/*.png')), 
-        ('share/batterymon/icons/default',
-                glob.glob('icons/default/*.png'))], 
+      data_files=[
+          ('share/batterymon/icons/16x16', glob.glob('icons/16x16/*.png')),
+          ('share/batterymon/icons/24x24_narrow', glob.glob('icons/24x24_narrow/*.png')),
+          ('share/batterymon/icons/24x24_wide', glob.glob('icons/24x24_wide/*.png')),
+          ('share/batterymon/icons/default', glob.glob('icons/default/*.png')),
+          ('share/batterymon/icons/gnome', glob.glob('icons/gnome/*.png')), 
+      ], 
       cmdclass = { "build" : build_extra.build_extra,
                    "build_i18n" :  build_i18n.build_i18n },
       )
