6eb7ebd277584e7c1e2ec40365231b3ad63132cf
Denilson Sá <denilsonsa@gmail.com>
2011-07-21 02:56:13 -0300

    Adding 3 icon themes: 16x16, 24x24_narrow, 24x24_wide
    I made these themes about 2 years ago and posted at batterymon issue
    list at Google Code:
    http://code.google.com/p/batterymon/issues/detail?id=5
    
    They are now finally added to batterymon-clone fork.

---
+++ icons_src/16x16/make_montage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+feh -m -Sfilename --bg trans --thumb-width 16 --thumb-height 16 -W 80 -O 16x16_montage.png batterymon_theme/*.png
---
+++ icons_src/24x24/make_montage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+feh -m -Sfilename --bg trans --thumb-width 24 --thumb-height 24 -W 120 -O 24x24_narrow_montage.png narrow_theme/*.png
+
+feh -m -Sfilename --bg trans --thumb-width 24 --thumb-height 24 -W 120 -O 24x24_wide_montage.png wide_theme/*.png
+
